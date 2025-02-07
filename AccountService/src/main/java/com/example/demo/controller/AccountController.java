28dd9c96e19c8e1ccb1e467456fc6e4e8f6b6202
medhakb <medhakb714@gmail.com>
2025-02-07 09:54:20 +0530

    Added print message

--- AccountService/src/main/java/com/example/demo/controller/AccountController.java
+++ AccountService/src/main/java/com/example/demo/controller/AccountController.java
@@ -20,6 +20,7 @@ import com.example.demo.service.AccountService;
 public class AccountController {
 	@Autowired
 	AccountService service;
+// System.out.println("Hello world!");
 
 	@PostMapping("/create")
 	public String saveAccount(@RequestBody Account account) {
